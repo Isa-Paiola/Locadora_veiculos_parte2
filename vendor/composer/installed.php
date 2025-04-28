fbe2d00b70a0d10eb72f95f49f8e867937161fe9
Isa-Paiola <isabelapaiola6@gmail.com>
2025-04-28 09:49:22 -0300

    commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'locadora/veiculos',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'c7205f56b4df863d4a5b57971f99943f7846b3f7',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'locadora/veiculos' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'c7205f56b4df863d4a5b57971f99943f7846b3f7',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
